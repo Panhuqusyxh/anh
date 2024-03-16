1ad55f245161ade5947425649d223c1c59ea3cc3
Panhuqusyxh <93659396+Panhuqusyxh@users.noreply.github.com>
2024-03-16 14:46:14 +0700

    Update tunnel-out.sh

--- tunnel-out.sh
+++ tunnel-out.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+[[ $EUID -ne 0 ]] && echo -e "${red}Lỗi:${plain} Cần chạy với quyền root!\n" && exit 1
+
+# Tải Gost
+wget -N --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-amd64-2.11.5.gz
+
+# Giải nén Gost
+gzip -d gost-linux-amd64-2.11.5.gz
+
+# Đổi tên tệp thực thi
+mv gost-linux-amd64-2.11.5 gost
+
+# Cấp quyền thực thi cho tệp Gost
+chmod 777 gost
